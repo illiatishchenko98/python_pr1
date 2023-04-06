8270d1b2a2c4375df5d358a5f4da712578324c2d
Illia Tishchenko <102582375+illiatishchenko98@users.noreply.github.com>
2023-04-06 11:47:01 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# def merge_list_to_dict(a, b):
+#     c = zip(a, b)
+#     return dict(c)
+
+
+# print(merge_list_to_dict([232, 13, 434], ['a', 'b', 'c']))
+
+
+def merge_list_to_dict(a, b):
+    c = zip(a, b)
+    return dict(c)
+
+
+print(merge_list_to_dict(['a', 'b', 'c'], [131, 133, 54354]))
